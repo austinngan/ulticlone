33f9d504806898cc01de8eaa1a01a9b7cf6dd0dd
yayeetying <yliangli20@stuy.edu>
2022-05-27 08:42:56 -0400

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,88 @@
-from flask import Flask
+from os import urandom
+from flask import Flask, render_template, request, session, redirect
+import sqlite3, os.path
+import json
+import urllib
+import random
+import db
+
 app = Flask(__name__)
+app.secret_key = urandom(32)
 
+def islogged():
+    return 'username' in session.keys()
 
-@app.route("/")
-def hello_world():
-	print("swagswagswag")
-	print(__name__)
-	return "yo"
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
+@app.route("/logout",  methods=['GET', 'POST'])
+def logout():
+    # try except is for when user is not logged in and does /logout anyways and a KeyError occurs
+    try:
+        session.pop('username')
+        session.pop('password')
+    except KeyError:
+        return redirect("/")
+    return redirect("/")
+
+#login takes the user object and sets cookies
+@app.route("/login",  methods=['GET', 'POST'])
+def login():
+	'''
+    # stops a loggedin user when they try to log in
+    if islogged():
+        return render_template('loggedlock.html')
+	'''
+    #create users table so user can login
+    db = sqlite3.connect("users.db")
+    c = db.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS users(email TEXT, password TEXT, name TEXT, usauID INT, UNIQUE(email))")
+    db.commit()
+    db.close()
+    return render_template('login.html')
 
 if __name__=="__main__":
 	app.debug = True
 	app.run()
+
+# authentication of login; verifies login information
+@app.route("/auth", methods=['GET', 'POST'])
+def auth():
+    if (request.method == 'POST'):
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        #error handling for empty username
+        if username == '':
+            return render_template("login.html", error="Empty username, who are you?")
+
+        db = sqlite3.connect('users.db')
+        c = db.cursor()
+        #in case users goes straight to /register w/o running /login code
+        c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, numRaces INT, numCoins INT, UNIQUE(username))")
+        c.execute("SELECT username FROM users WHERE username=? ", (username,)) #SYNTAX: ADD , after to refer to entire username, otherwise SQL will count each char as a binding... -_-
+        # username inputted by user is not found in database
+        if c.fetchone() == None:
+            return render_template("login.html", error="Wrong username, double check spelling or register")
+        # username is found
+        else:
+            c.execute("SELECT password FROM users WHERE username=? ", (username,))
+            # password associated with username in database does not match password inputted
+
+            #c.fetchone() returns a tuple with the password
+            #first convert the tuple into the password string only, then compare
+            if ( ''.join(c.fetchone()) ) != password:
+                return render_template("login.html", error="Wrong password")
+            # password is correct
+            else:
+                session['username'] = username
+                session['password'] = password
+                print(session['username'])
+        db.close()
+        return redirect('/')
+
+    #get method
+    else:
+        return redirect('/login')
