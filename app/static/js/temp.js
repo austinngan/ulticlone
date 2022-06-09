46d45f8398565e8a7c437831f31a607e0b20aae4
Yoonah Chang <ichang20@stuy.edu>
2022-06-09 18:34:17 -0400

    tryna work on plays

---
+++ app/static/js/temp.js
@@ -0,0 +1,61 @@
+/* Stinky Fingers: Austin Ngan (Gerald), Yoonah Chang (Yelena), Yaying Liang Li (Blob), Han Zhang (Sirap)
+SoftDev
+P04 -- Ultibase
+2022-06-10 */
+
+var c = document.getElementById("playground");
+
+var ctx = c.getContext("2d");
+
+var mode = "rect";
+var toggler = document.getElementById("draw");
+
+var toggleMode = (e) => {
+    if (mode == "rect") {
+        mode = "circle";
+        toggler.innerHTML = "Circle"
+    } else {
+        mode = "rect";
+        toggler.innerHTML = "Rect"
+    }
+}
+
+toggler.addEventListener("click", toggleMode);
+
+var draw = (e) => {
+    if (mode == "rect") {
+       drawRect(e);
+    } else {
+       drawCircle(e);
+    }
+}
+
+var drawRect = (e) => {
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+
+    ctx.rect(mouseX, mouseY, 100, 150)
+    ctx.fillStyle = "green"
+    ctx.fill()
+}
+
+var drawCircle = (e) => {
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+
+    ctx.beginPath()
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI)
+    ctx.fillStyle = "blue"
+    ctx.fill()
+    ctx.stroke()
+}
+
+var clearButton = document.getElementById("clear")
+
+var wipeCanvas = function() {
+    ctx.clearRect(0, 0, c.clientWidth, c.height)
+}
+
+clearButton.addEventListener("click", wipeCanvas);
+
+c.addEventListener("click", draw)
